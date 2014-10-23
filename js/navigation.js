cd906dc63f8d41f97c8e8122f671d0f9081b6e69
Michael Zhang <failed.down@gmail.com>
2014-10-22 21:14:20 -0500

    includes the navigation bar now

--- js/navigation.js
+++ js/navigation.js
@@ -1,7 +1,17 @@
 var Navigation = {
     Init: function() {
     	if ("import" in document.createElement("link")) {
-
+    		Navigation.ImportElement = document.createElement("link");
+    		Navigation.ImportElement.rel = "import";
+    		Navigation.ImportElement.href = "include/navigation.html";
+    		Navigation.ImportElement.onload = function(e) {
+    			// cb
+    		};
+    		Navigation.ImportElement.onerror = function(e) {
+    			// something went wrong
+    			console.dir(e);
+    		};
+    		document.head.appendChild(Navigation.ImportElement);
     	} else {
     		// not supported
     		console.log("Your browser is not supported (a.k.a. stop using IE 6)");
