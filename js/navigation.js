7f89a69e5ed8ea221e7caf47a0e9c25d59bd4878
Michael Zhang <failed.down@gmail.com>
2014-10-22 21:32:46 -0500

    lol using some old code

--- js/navigation.js
+++ js/navigation.js
@@ -1,11 +1,21 @@
 var Navigation = {
-    Init: function() {
-		Navigation.ImportElement = document.querySelector("link[rel=import]");
-		Navigation.Content = Navigation.ImportElement.import;
-		console.dir(Navigation.Content);
-    },
+	Check: function(callback) {
+		if (!("import" in document.createElement("link"))) {
+			console.log("Import is not supported in this browser! Time to upgrade your browser.");
+		} else {
+			console.log("Import is supported in this browser! Congratulations!");
+		}
+		callback();
+	},
+	Load: function() {
+		var content = document.querySelector("link[rel='import']").import;
+		var el = content.querySelector("div#navigation-import");
+		document.body.appendChild(el.cloneNode(true));
+	},
 };
 
 $(document).ready(function() {
-	Navigation.Init();
+	Navigation.Check(function() {
+		Navigation.Load();
+	});
 });
