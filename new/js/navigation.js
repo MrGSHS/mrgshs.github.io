489121a91997936155b9abc8f3a379c4ff342bae
Michael Zhang <failed.down@gmail.com>
2014-10-23 00:53:23 -0500

    created alternative for html5 import

--- new/js/navigation.js
+++ new/js/navigation.js
@@ -2,20 +2,36 @@ var Navigation = {
 	Check: function(callback) {
 		if (!("import" in document.createElement("link"))) {
 			console.log("Import is not supported in this browser! Time to upgrade your browser.");
+			callback(false);
 		} else {
 			console.log("Import is supported in this browser! Congratulations!");
+			callback(true);
 		}
-		callback();
 	},
-	Load: function() {
-		var content = document.querySelector("link[rel='import']").import;
-		var el = content.querySelector("div#navigation-import");
-		document.body.appendChild(el.cloneNode(true));
+	Load: function(supported) {
+		if (supported) {
+			var content = document.querySelector("link[rel='import']").import;
+			var el = content.querySelector("div#navigation-import");
+			document.body.appendChild(el.cloneNode(true));
+		} else {
+			var url = document.getElementById("navigation-import").getAttribute("href");
+			$.ajax({
+				url: url,
+				dataType: "html",
+				type: "get",
+				success: function(content) {
+					var el = document.createElement("div");
+					el.innerHTML = content;
+
+					document.body.appendChild(el.cloneNode(true));
+				}
+			});
+		}
 	},
 };
 
 $(document).ready(function() {
-	Navigation.Check(function() {
-		Navigation.Load();
+	Navigation.Check(function(supported) {
+		Navigation.Load(supported);
 	});
 });
