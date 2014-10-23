5ab725876cd9a8afe42828e672f910593960b648
Michael Zhang <failed.down@gmail.com>
2014-10-22 21:12:10 -0500

    check for compatibility

--- js/navigation.js
+++ js/navigation.js
@@ -1,6 +1,14 @@
 var Navigation = {
     Init: function() {
+    	if ("import" in document.createElement("link")) {
 
+    	} else {
+    		// not supported
+    		console.log("Your browser is not supported (a.k.a. stop using IE 6)");
+    	}
     },
 };
 
+$(document).ready(function() {
+	Navigation.Init();
+});
